7c6528adb4209141b33027c02de6f8edf30dbc4f
rafaeltavaares <rafaeltavsilva@gmail.com>
2025-07-29 22:06:26 -0300

    chore: removing lombok because of bugs

--- api/src/main/java/com/rede/demo/domain/Engajamento.java
+++ api/src/main/java/com/rede/demo/domain/Engajamento.java
@@ -11,12 +11,9 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Engajamento {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @ManyToOne
@@ -31,4 +28,62 @@ public class Engajamento {
     private Taxonomia taxonomia;
 
     private LocalDateTime criado_em;
+
+    public Engajamento(Usuario usuario, Postagem postagem, Taxonomia taxonomia ){
+        this.usuario = usuario;
+        this.postagem = postagem;
+        this.taxonomia = taxonomia;
+        this.criado_em = LocalDateTime.now();
+    }
+
+    public Engajamento() {
+    }
+
+    public Engajamento(Long id, Usuario usuario, Postagem postagem, Taxonomia taxonomia, LocalDateTime criado_em) {
+        this.id = id;
+        this.usuario = usuario;
+        this.postagem = postagem;
+        this.taxonomia = taxonomia;
+        this.criado_em = criado_em;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Postagem getPostagem() {
+        return postagem;
+    }
+
+    public void setPostagem(Postagem postagem) {
+        this.postagem = postagem;
+    }
+
+    public Taxonomia getTaxonomia() {
+        return taxonomia;
+    }
+
+    public void setTaxonomia(Taxonomia taxonomia) {
+        this.taxonomia = taxonomia;
+    }
+
+    public LocalDateTime getCriado_em() {
+        return criado_em;
+    }
+
+    public void setCriado_em(LocalDateTime criado_em) {
+        this.criado_em = criado_em;
+    }
 }
