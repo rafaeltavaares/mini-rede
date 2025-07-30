89a99f917a06ed3e30cec684bb5d59356aae9ccb
rafaeltavaares <rafaeltavsilva@gmail.com>
2025-07-29 22:10:33 -0300

    feat: Creating JPA method to access which post the user has an interaction

--- api/src/main/java/com/rede/demo/repositories/EngajamentoRepository.java
+++ api/src/main/java/com/rede/demo/repositories/EngajamentoRepository.java
@@ -2,10 +2,19 @@ package com.rede.demo.repositories;
 
 import com.rede.demo.domain.Amizade;
 import com.rede.demo.domain.Engajamento;
+import com.rede.demo.domain.Flags.Taxonomia;
+import com.rede.demo.domain.Postagem;
+import com.rede.demo.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface EngajamentoRepository extends JpaRepository<Engajamento,Long> {
+
+    Optional<Engajamento> findEngajamentoByUsuarioAndPostagemAndTaxonomia(Usuario usuario, Postagem postagem, Taxonomia taxonomia);
+    List<Engajamento> findEngajamentoByUsuarioAndPostagem(Usuario usuario, Postagem postagem);
     
 }
